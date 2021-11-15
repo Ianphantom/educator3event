069746198113fb9ce62b9b50e3920d45bd04d86d
Ianphantom <ian25yola@gmail.com>
2021-11-15 11:53:19 +0700

    Pembuatan routes untuk URL

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('register', 'AccountCtl::index');
+$routes->get('login', 'AccountCtl::login');
+$routes->get('register', 'AccountCtl::register');
+$routes->get('dashboard/profile', 'DashboardCtl::profile');
 
 /*
  * --------------------------------------------------------------------
