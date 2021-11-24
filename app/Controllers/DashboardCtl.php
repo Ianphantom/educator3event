c677b98e3312c2ad9d881ed46903b587b80e47fc
Ianphantom <ian25yola@gmail.com>
2021-11-24 11:12:25 +0700

    Profile bank

--- app/Controllers/DashboardCtl.php
+++ app/Controllers/DashboardCtl.php
@@ -4,20 +4,20 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\AccountModel;
+use App\Models\BankModel;
 
 class DashboardCtl extends BaseController
 {
     public function profile()
     {
         $AccountModel = new AccountModel();
-        // $dashboard = $AccountModel->where('id', session()->get('loggedUser'));
-        // $dashboard = $AccountModel->getWhere(['id' => session()->get('loggedUser')]) -> getResult();
-        $dashboard = $AccountModel->getUserData(3);
-        // foreach($dashboard as $dash){
-        //     echo $dash->nama_lengkap;
-        // }
+        $bankModel = new BankModel();
+        $id = session()->get('loggedUser');
+        $dashboard = $AccountModel->getUserData($id);
+        $bank = $bankModel->findAll();
         $data = [
-            'data' => $dashboard
+            'data' => $dashboard,
+            'banks' => $bank,
         ];
         return view('dashboard/profile', $data);
     }
