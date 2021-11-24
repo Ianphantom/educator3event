a6933f0c2b451395afeeede367396df8da397ec5
Ianphantom <ian25yola@gmail.com>
2021-11-24 19:20:35 +0700

    editing event

--- app/Controllers/AdminCtl.php
+++ app/Controllers/AdminCtl.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\AdminModel;
+use App\Models\EventModel;
 
 class AdminCtl extends BaseController
 {
@@ -34,6 +35,27 @@ class AdminCtl extends BaseController
 
     public function event()
     {
-        return view('Panel/event/index');
+        $eventModel = new EventModel();
+        $event = $eventModel->findAll();
+
+        $data = [
+            'event' => $event
+        ];
+        return view('Panel/event/index', $data);
+    }
+
+    public function editEvent($seg1 = false)
+    {
+        $eventModel = new EventModel();
+        $event = $eventModel->where("id", $seg1)->first();
+        $data = [
+            'event' => $event
+        ];
+        return view('Panel/event/edit', $data);
+    }
+
+    public function inputEvent()
+    {
+        return view('Panel/event/input');
     }
 }
