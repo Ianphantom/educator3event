90030716f996d43960b0379685e3f8d7f822ae14
Ianphantom <ian25yola@gmail.com>
2021-11-24 17:42:15 +0700

    event model

--- app/Models/EventModel.php
+++ app/Models/EventModel.php
@@ -1,10 +1,42 @@
-<?php 
+<?php
 
 namespace App\Models;
 
 use CodeIgniter\Model;
 
-class EventModel extends Model 
+class EventModel extends Model
 {
-    protected $table = 'event';
+    protected $DBGroup          = 'default';
+    protected $table            = 'event';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ["nama_eo","nama_acara","deskripsi","kuota","harga_normal","harga_referral", "komisi_referral","updated_at", "deleted_at"];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 }
